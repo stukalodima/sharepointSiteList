68f2d4658ca247a84aed9970c3f1d0b196aae0e3
dima <dmytro.stukalo@it-capital.com.ua>
2024-09-22 03:16:59 +0300

    dep

--- sharepointSiteList/settings.py
+++ sharepointSiteList/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sharepointSiteList.urls'
